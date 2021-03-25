810db69a50633e330b30b2e93b6b569e8efc2c65
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2021-03-25 19:49:59 -0300

    ...

--- stream/stream.py
+++ stream/stream.py
@@ -4,13 +4,9 @@ import random
 import fileHandle
 
 lines = fileHandle.getLines("e:/persistence.txt")
-random.shuffle(lines)
 fname = os.environ.get('TEMP')+"\\test.txt"
 
-#os.system("youtube-dl -g "+lines[0]+" > "+fname)
-
-fileHandle.shuffleLines(lines,fname)
 while True:
-    #-I dummy --dummy-quiet -vvv
-    os.system("vlc -I dummy --dummy-quiet -vvv "+fname+" :sout=#http{dst=192.168.0.169,port=8080,mux=ts,ttl=1} :sout-all :sout-keep vlc://exit")
+    random.shuffle(lines)
+    os.system("youtube-dl --get-url --format best "+lines[0]+" | vlc -I dummy --dummy-quiet - :sout=#http{dst=192.168.0.169,port=8080,mux=ts,ttl=1} :sout-all :sout-keep vlc://quit")
     time.sleep(1/1000)
