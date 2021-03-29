95505a7f770276db00c13b8996dd39646e6fc151
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2021-03-28 22:47:53 -0300

    ...

--- rememberme/test.py
+++ rememberme/test.py
@@ -1,8 +1,16 @@
 import fileHandle
+import datetime
+import datetime
 
+now = datetime.datetime.now()
+now = str(now)
+date = now.split(" ")[0]
+time = now.split(" ")[1].split(".")[0]
+print(date)
+print(time)
 print("thing to register:")
 register = input()
 
-fileHandle.createFile("register")
-fileHandle.writeFile("register",register)
+#fileHandle.createFile(date)
+fileHandle.writeFile(date,time+":"+register)
 
