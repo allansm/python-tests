41cdd111a866d340803f345b87a031d787119047
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-06 15:51:27 -0300

    ..

--- ytplayList/test.py
+++ ytplayList/test.py
@@ -11,11 +11,29 @@ def play():
         system("youtube-dl -x --audio-format mp3 "+line)
         mp3 = ls(".","*.mp3")[0]
         system("ffplay -nodisp -autoexit \""+mp3+"\"")
-        
+        remove(mp3)
+
+def useFile(fname):
+    if(not fname.startswith("http")):
+        if(exists(fname)):
+            lines = getLines(fname)
+
+            for line in lines:
+                if(not line.startswith("https://www.youtube.com/playlist?list=")):
+                    list = getListLink(line)
+                else:
+                    list = line
+
+                getLinksFromList(list)
+                play()
+            exit()
+
 def console():
     remove("persistence.txt")
-    link = input("playlist link:")
-    
+    link = input("playlist link or txt path:")
+
+    useFile(link)
+
     if(not link.startswith("https://www.youtube.com/playlist?list=")):
         list = getListLink(link)
     else:
