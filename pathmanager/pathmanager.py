1da2992f22a31e1e0dcabb28bb0fc068433568ff
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-15 20:20:16 -0300

    ...

---
+++ pathmanager/pathmanager.py
@@ -0,0 +1,35 @@
+import sys
+
+sys.path.append("../functions")
+
+from fileHandle import *
+
+from os import chdir
+from os import system
+
+
+def run():
+    paths = getLines(".config")
+
+    command = input("command:")
+
+    if(command == "select"):
+        i=0
+        for path in paths:
+            print(str(i)+" : "+path)
+            i = i+1
+
+        index = input("index :")
+
+        chdir(paths[int(index)])
+        
+        command = input("command:")
+
+        system(command)
+    
+    else:
+        for path in paths:
+            chdir(path)
+            system(command)
+
+run()
