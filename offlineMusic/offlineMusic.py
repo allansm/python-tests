05d49f67b5e0207e3ae842c64d1f5e1b724edc59
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-20 17:51:38 -0300

    ...

--- offlineMusic/offlineMusic.py
+++ offlineMusic/offlineMusic.py
@@ -1,43 +1,64 @@
 from os import system
+from os import chdir
+from os import mkdir
+from random import shuffle
+from shutil import copyfile
+
 
 import sys
 sys.path.append("../functions")
 
 from fileHandle import *
-from timeHandle import * 
+from timeHandle import *
 
-fname = getTemp()+"\\music.txt"
-path = input("music path:")
+def createList(mpath):
+    if(exists("persistence.txt")):
+        copyfile("persistence.txt","music.txt")
+    else:
+        files = ls(mpath,"*.mp3")
 
-def getMusic(folder):
-    log = getTemp()+"\\offlinemusic.log";
-    writeFile(log,getDate()+" - "+getTime()+" refill \n")
+        shuffle(files)
+        shuffle(files)
+        shuffle(files)
 
-    fname = getTemp()+"\\music.txt"
-    remove(fname)   
-
-    ltos = ls(folder,"*.mp3")    
-
-    writeLines(fname,ltos)
-
-    lines = getLines(fname)
-
-    shuffleLines(lines,fname)
-
-    for line in lines:
-        print(line)
-
-if isEmpty(fname):
-    getMusic(path)
+        remove("music.txt")
+        writeLines("music.txt",files)
+        writeFile("music.log","new list created\n")
+        copyfile("music.txt","persistence.txt")
 
 
-while True:
-    if isEmpty(fname):
-        getMusic(path)
+mpath = input("music path:")
 
-    if exists(fname):
-        try:
-            system("ffplay -autoexit -nodisp \""+consumeLineAndShuffle(fname,0)+"\"")
-        
-        except:
-            print("file not found!!")
+chdir(getTemp())
+
+
+
+try:
+    mkdir("offmusic")
+
+except:
+    print("folder exists..\n")
+
+chdir("offmusic")
+
+try:
+    if(mpath != getLines("last.txt")[0]):
+        remove("music.txt")
+        remove("persistence.txt")
+except:
+    print("error!!")
+
+writeFile("last.txt",mpath)
+
+
+
+while(True):
+    if isEmpty("music.txt"):
+        createList(mpath)
+
+    mp3 = consumeLine("music.txt",0)
+    
+    writeFile("music.log",removeBreakLine(mp3)+"\n")
+    
+    system("ffplay -autoexit -nodisp \""+mp3+"\"")
+
