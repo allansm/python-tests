63b73fa340cfb0239315573f2efbe57f65b1f23b
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-21 17:42:03 -0300

    ...

--- test/scshot.py
+++ test/scshot.py
@@ -1,9 +1,17 @@
-import wx
-wx.App()  # Need to create an App instance before doing anything
-screen = wx.ScreenDC()
-size = screen.GetSize()
-bmp = wx.EmptyBitmap(size[0], size[1])
-mem = wx.MemoryDC(bmp)
-mem.Blit(0, 0, size[0], size[1], screen, 0, 0)
-del mem  # Release bitmap
-bmp.SaveFile('screenshot.png', wx.BITMAP_TYPE_PNG)
+from os import chdir
+from os import system
+
+import sys
+
+sys.path.append("modules/pillow-8.2.0/src/pil")
+sys.path.append("modules/pillow-8.2.0/src")
+
+from PIL import ImageGrab
+
+chdir("modules/pillow-8.2.0/src/pil")
+
+filepath = 'my_image.png'
+
+screenshot = ImageGrab.grab()
+screenshot.save(filepath, 'PNG')  
+
