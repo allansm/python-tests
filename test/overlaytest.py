7012e73bebcf0339b63e534c1a469f813ea73efa
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-29 20:57:32 -0300

    ...

---
+++ test/overlaytest.py
@@ -0,0 +1,21 @@
+
+import tkinter as tk
+
+
+root=tk.Tk()
+
+root.overrideredirect(1)
+
+#txt = StringVar()
+
+#txt.set("helloworld")
+
+label = tk.Label(root, text="helloworld")
+root.overrideredirect(True)
+root.geometry("+250+250")
+#root.lift()
+root.wm_attributes("-topmost", True)
+#root.wm_attributes("-disabled", True)
+#root.wm_attributes("-transparentcolor", "white")
+label.pack()
+label.mainloop()
