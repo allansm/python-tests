28b76573ca4610a4cac7dad4a8dd6f1f6491a3e3
allansm <allan.de.souza.melo.rj@gmail.com>
2021-05-19 16:37:38 -0300

    ...

--- pathmanager/pathselect.py
+++ pathmanager/pathselect.py
@@ -19,7 +19,19 @@ def run():
     chdir(paths[int(index)])
     
     command = input("command:")
+    if(command == "select"):
+        i = 0
+        paths2 = ls(".","*")
+        for path in paths2:
+            print(str(i)+" : "+path)
+            i = i+1
 
+        index = input("index :")
+
+        chdir(paths2[int(index)])
+        
+        command = input("command:")
+    
     system(command)
 
 run()
