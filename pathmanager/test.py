864be4add99e8c7b419781a2451dd595ef174251
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-03 00:34:26 -0300

    ...

--- pathmanager/test.py
+++ pathmanager/test.py
@@ -8,10 +8,7 @@ from os import chdir
 from os import system
 from os import getcwd
 
-def run():
-    flag = False
-
-    paths = getLines(".config")
+def select(paths):
     i=0
     for path in paths:
         print(str(i)+" : "+path)
@@ -20,12 +17,31 @@ def run():
     index = input("index :")
 
     chdir(paths[int(index)])
-    
+
+
+def run():
+    flag = False
+
+    paths = getLines(".config")
+    '''
+    i=0
+    for path in paths:
+        print(str(i)+" : "+path)
+        i = i+1
+
+    index = input("index :")
+
+    chdir(paths[int(index)])
+    '''
+    select(paths)
+
     while(True):
-        command = input(getcwd().replace(":\\",".").replace("\\",".").replace(" ","")+".")
+        package = getcwd().split(":\\")[1].replace("\\",".").replace(" ","")
+        command = input("."+package+".")
         if("cd " in command):
             chdir(command.replace("cd ",""))
         elif(command == "select"):
+            '''
             i=0
             for path in paths:
                 print(str(i)+" : "+path)
@@ -34,7 +50,8 @@ def run():
             index = input("index :")
 
             chdir(paths[int(index)])
-        
+            '''
+            select(paths)
         else:
             system(command) 
 run()
