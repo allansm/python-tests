e6e0e696a0f4f58c65ef21c86828630e9a7f5cd8
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-15 18:48:31 -0300

    ...

--- search/find.py
+++ search/find.py
@@ -3,16 +3,22 @@ sys.path.append("../functions")
 import os
 from argsHandle import *
 
-args = getArgs(["path","text"],"")
+args = getArgs(["path","text","ext"],"")
 
 path = args.path
 text = args.text
+ext = args.ext
 
 fold = os.walk(path)
 for root, dirs, files in fold:
     for name in files:
         tmp = os.path.realpath(os.path.join(root, name))
-        if(text in tmp):
-            print(tmp)
+        if(text.lower() in tmp.lower()):
+            if(ext != None):
+                if(ext.lower() in tmp.lower()):
+                    print(tmp)
+
+            else:
+                print(tmp)
 
 
