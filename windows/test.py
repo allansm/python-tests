6b490b7dddc1ac02b1b85439202298fdd580d5e3
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-17 21:38:40 -0300

    ...

--- windows/test.py
+++ windows/test.py
@@ -1,17 +1,31 @@
+import sys
+sys.path.append("../functions")
+
 from subprocess import check_output
+from time import sleep
+from util import *
 
-output = check_output("tasklist",shell=True)
+def inspect(limit):
+    output = check_output("tasklist",shell=True)
 
-list = str(output).split("\\r\\n")
+    list = str(output).split("\\r\\n")
 
-for item in list:
-    if(item != "" and " K" in item):
-        array = item.split(" ")
+    for item in list:
+        if(item != "" and " K" in item):
+            array = item.split(" ")
 
-        array = [i for i in array if i]
-        
-        exe = array[0]
-        pid = array[1]
-        memory = array[4].replace(".","")
+            array = [i for i in array if i]
+            
+            if("exe" in array[0]):
+                exe = array[0]
+                pid = array[1]
+                memory = array[4].replace(".","")
+                
 
-        print(exe+ " "+memory)
+                if(int(memory) > limit):
+                    print(exe)
+
+while(True):
+    inspect(50000)
+    sleep(5)
+    clear()
