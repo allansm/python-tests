713f0074db41443f74bde209fb57c782f13fe126
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-20 21:37:05 -0300

    ...

--- windows/task.py
+++ windows/task.py
@@ -1,13 +1,15 @@
 import sys
 sys.path.append("../functions")
 
-from subprocess import check_output
-from subprocess import call
-from os import system
+#from subprocess import check_output
+#from subprocess import call
+#from os import system
 from time import sleep
 from util import *
 from argsHandle import *
+from proc import *
 
+'''
 def inspect(limit,op,pattern):
     pk = False
     tokill = []
@@ -65,6 +67,61 @@ def inspect(limit,op,pattern):
     print("memory by detection:"+str(totaldetmem/1000))
     print("n:"+str(totalexe))
     print("total detections:"+str(totaldet))
+'''
+
+def test(limit,op,pattern):
+    pk = False
+    tokill = []
+    if(pattern != None):
+        pk = True
+        tokill = pattern.split(";")
+        
+
+    totalmem = 0
+    totalexe = 0
+    totaldet = 0
+    totaldetmem = 0
+    
+    if(limit == None):
+        limit = 50000
+    else:
+        limit = int(limit)
+
+
+    i = 0
+    out = getOutput()
+
+    nproc = getProcess(out)
+    npid = getPid(out)
+    nmem = getMemory(out)
+
+    for proc in nproc:
+        exe = proc
+        pid = npid[i]
+        memory = nmem[i]
+
+        i = i + 1
+        if(int(memory) > limit):
+            print(exe)
+            
+            if(op == "kill"):
+                kill(pid)
+            if(pk):
+                for p in tokill:
+                    if(p.lower() in exe.lower()):
+                        kill(pid)
+            totaldet = totaldet + 1
+            totaldetmem = totaldetmem + int(memory)
+
+        totalmem = totalmem + int(memory)
+        totalexe = totalexe + 1
+
+
+    print("")
+    print("memory in use:"+str(totalmem/1000))
+    print("memory by detection:"+str(totaldetmem/1000))
+    print("n:"+str(totalexe))
+    print("total detections:"+str(totaldet))
      
 def console():
     clear()
@@ -80,7 +137,9 @@ def console():
     else:
         interval = int(interval)
 
-    inspect(limit,op,pattern)
+    #inspect(limit,op,pattern)
+    test(limit,op,pattern)
+
     sleep(interval)
     
 while(True):
