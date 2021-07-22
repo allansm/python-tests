1221f90597ebdef6fb465598ee13fe56b477b617
allansm <allan.de.souza.melo.rj@gmail.com>
2021-07-21 21:25:39 -0300

    ...

---
+++ rename/test.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.path.append("../functions")
+
+from fileHandle import *
+from os import chdir
+from os import rename
+from argsHandle import *
+
+def run():
+    args = getArgs(["folder","currentname","newname"])
+
+    folder = args.folder
+    cname = args.currentname
+    nname = args.newname
+
+    chdir(folder)
+    files = getAllFiles(".")
+
+    for tmp in files:
+        newname = tmp.replace(cname,nname)
+        
+        #rename(tmp,newname)
+
+        print(tmp+" > "+newname)
+
+
+run()
