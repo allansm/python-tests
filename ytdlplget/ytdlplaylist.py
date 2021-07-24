33c1e608efc1dc348e4ecd2b44ffe0307e07720e
allansm <allan.de.souza.melo.rj@gmail.com>
2021-07-24 19:38:33 -0300

    ...

--- ytdlplget/ytdlplaylist.py
+++ ytdlplget/ytdlplaylist.py
@@ -5,28 +5,25 @@ from fileHandle import *
 from re import search
 from os import system
 from youtube import *
+from os import chdir
+from os import mkdir
+from os import getcwd
 
-#deprecated
-'''
-def console():
-    print("starting..\n")
-    remove("persistence.txt")
-
-    link = input("playlist link:")
-    
-    if(not link.startswith("https://www.youtube.com/playlist?list=")):
-        list = getListLink(link)
-    else:
-        list = link
-
-    getLinksFromList(list)
-
-    downloadAsMusic()
-'''
 def console():
     info = getInfo(input("playlist link:"))
 
     links = getLinks(info)
+    
+    chdir(getTemp())
+
+    try:
+        mkdir("ytdownloads")
+    except:
+        dummy = ""
+
+    chdir("ytdownloads")
+    
+    print("your files will be downloaded in :"+getcwd())
 
     for link in links:
         print("downloading : "+link+"...")
