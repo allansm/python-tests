fea2dca69dd72a00cc456fd8923b22192bba9aac
allansm <allan.de.souza.melo.rj@gmail.com>
2021-08-12 11:58:37 -0300

    ...

--- test/fixmeitest.py
+++ test/fixmeitest.py
@@ -3,9 +3,11 @@ sys.path.append("../functions")
 
 from fileHandle import *
 import os
+from shutil import rmtree
 
-arr = ls(getTemp(),"*")
+def removeMei():
+    arr = ls(getTemp(),"*")
 
-for a in arr:
-    if("MEI" in a and "_" in a):
-        print(a)
+    for a in arr:
+        if("MEI" in a and "_" in a):
+            rmtree(a, ignore_errors=True)
