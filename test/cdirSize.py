d5c42e248ecad85274db5d500c6ce9647c6b90a4
allansm <allan.de.souza.melo.rj@gmail.com>
2021-08-14 13:19:09 -0300

    ...

---
+++ test/cdirSize.py
@@ -0,0 +1,20 @@
+import os
+tmp = os.getcwd()
+
+os.chdir(os.path.dirname(__file__))
+
+import sys
+sys.path.append("../functions")
+
+from fileHandle import *
+
+os.chdir(tmp)
+
+size = 0
+
+files = getAllFilesPath(".")
+
+for f in files:
+    size += os.path.getsize(f)
+
+print(size)
