dd57e6dde12d8c9f6aac6afd1e6ab91c7728d741
allansm <allan.de.souza.melo.rj@gmail.com>
2021-08-20 12:14:50 -0300

    ...

---
+++ pathmanager/test.py
@@ -0,0 +1,88 @@
+import sys
+sys.path.append("../functions")
+
+from fileHandle import ls
+from argsHandle import *
+from util import showOptions as show
+from os import chdir
+from os import system
+from os import getcwd
+from os.path import isdir
+
+def slow(path,before,tmp):
+    chdir(path)
+    while(True):
+        print("")
+        print("-------------------------------------------------------------------------")
+
+        if(before != None and before != ""):
+            system(before)
+
+        print("")
+        print("     PATH:"+path)
+        print("")
+
+        command = input("command:")
+        
+        if(command == "exit"):
+            chdir(tmp)
+            break;
+
+        system(command)
+
+def fast(path,command,tmp):
+    chdir(path)
+    print("")
+    print("-------------------------------------------------------------------------")
+    '''
+    if(before != None and before != ""):
+        system(before)
+    '''
+    print("")
+    print("     PATH:"+path+"")
+    print("")
+    system(command)
+    chdir(tmp)
+
+def run():
+    args = getArgs(["path","--command","--mode"])
+    path = args.path
+    before = args.command
+    mode = args.mode
+    
+    if(mode == None and before == None):
+        before = input("command:")
+
+    tmp = getcwd()
+
+    paths = ls(path,"*")
+    #show(paths)
+    
+    for path in paths:
+        if(path != "" and isdir(path)):
+            if(mode == "slow"):
+                slow(path,before,tmp)
+            else:
+                fast(path,before,tmp) 
+            '''
+            chdir(path)
+            while(True):
+                print("")
+                print("-------------------------------------------------------------------------")
+
+                if(before != None and before != ""):
+                    system(before)
+
+                print("")
+                print("     PATH:"+path)
+                print("")
+
+                command = input("command:")
+                
+                if(command == "exit"):
+                    chdir(tmp)
+                    break;
+
+                system(command)'''
+
+run()
