3138ae0154d2e4d2200c27acc9087589afca9fd1
allansm <allan.de.souza.melo.rj@gmail.com>
2021-08-26 12:33:29 -0300

    ...

--- pathmanager/localgit.py
+++ pathmanager/localgit.py
@@ -17,22 +17,28 @@ paths = getLines(".config")
 totup = 0
 totcom = 0
 
-show = getArgs(["?show"]).show
+args = getArgs(["?show","?who"])
+show = args.show
+who = args.who
+
+modified = []
 
 print("")
 
 for path in paths:
     if(path != ""):
         chdir(path)
+        
         if(show):
             print(" "+path)
+
         output = subprocess.check_output("git status", shell=True)
 
         if("Changes not staged for commit" in str(output) or "Untracked files" in str(output)):
             if(show):
                 print("     need update\n")
             totup = totup+1
-
+            modified.append(path)
         else:
             if(show):
                 print("     all up to date\n")
@@ -40,7 +46,19 @@ for path in paths:
         if("Your branch is ahead of" in str(output) and "by" in str(output) and "(use \"git push\" to publish your local commits)" in str(output)):
             tmp = str(output).split("by")[1].split(" ")[1]
             tmp = int(tmp)
-            totcom += tmp 
+            totcom += tmp
+            modified.append(path)
+if(who):
+    for mod in modified:
+        if "\\" in mod:
+            proj = mod.split("\\")[-1]
+        elif "/" in mod:
+            proj = mod.split("/")[-1]
+        else:
+            proj = mod
+
+        print(proj)
+    exit()
 
 print("total projects to be updated:"+str(totup))
 print("total updates:"+str(totcom))
