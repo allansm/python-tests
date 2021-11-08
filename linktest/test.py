e073773617ba4dcf437af059fdc6653baba6760c
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-07 22:14:39 -0300

    ...

--- linktest/test.py
+++ linktest/test.py
@@ -1,9 +1,12 @@
 import dependency
 
-from os import mkdir,chdir,symlink
+from os import mkdir,chdir,symlink,getcwd
 from fileHandle import *
 from argsHandle import *
 from shutil import rmtree
+
+#print(getcwd())
+#exit()
 
 args = getArgs(["dir","--out"])
 files = getAllFilesPath(args.dir)
@@ -20,9 +23,11 @@ def build(dn):
         dummy = ""
 odir = ""
 if(args.out != None): 
-    build(args.out)
-    chdir(args.out)
-    odir=args.out
+    #build(args.out)
+    #chdir(args.out)
+    #odir=args.out
+    print(":O")
+    exit()
 else: 
     build("links")
     chdir("links")
@@ -30,19 +35,25 @@ else:
 
 
 for f in files:
-    fn = getFileName(f)
-    tmp = f
-    if(":\\" in tmp):
-        tmp = tmp.split(":\\")[1]
-    elif(":/" in tmp):
-        tmp = tmp.split(":/")[1]
-
-    tmp = tmp.replace(args.dir,odir)
-    filepath = tmp
-    folder = tmp.replace(fn,"")
-    print(folder)
-    try:
-        mkdir(folder)
-    except:
-        dummy=""
-    #os.symlink(f,filepath)
+    if(not ":" in args.dir):
+        fn = getFileName(f)
+        tmp = "" 
+        if(args.dir+"/" in f):
+            tmp = f.split(args.dir+"/")[1]
+        elif(args.dir+"\\" in f):
+            tmp = f.split(args.dir+"\\")[1]
+        
+        tmp2 = tmp
+        tmp = tmp.replace(fn,"")
+        
+        try:
+            if(not tmp == "\\" and not tmp == "/" and not tmp == ""): 
+                print(tmp)
+                mkdir(tmp)
+        except:
+            dummy=""
+        
+        try:
+            os.symlink(f,tmp2)
+        except:
+            dummy=""
