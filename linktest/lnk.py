dd5850e1c058dea737cc6a0f13b972475806e9d2
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-10 22:42:55 -0300

    ...

--- linktest/lnk.py
+++ linktest/lnk.py
@@ -1,6 +1,6 @@
 import dependency
 
-def build(dn):
+def rebuild(dn):
     from shutil import rmtree
     from os import mkdir
 
@@ -21,10 +21,10 @@ def getPaths(args):
     files = getAllFilesPath(args.dir)
     
     tmp = getcwd()
-    #print(tmp)
+    
     chdir(args.dir)
     dir = getcwd()
-    #print(dir)
+    
     chdir(tmp)
 
     farr = []
@@ -43,19 +43,7 @@ def getPaths(args):
             tok = dir.split("\\")[-1]+"\\"
             if(tok in f):
                 tmp = f.split(tok)[1]
-        #else:
-        #    print(":O")
-        '''else:
-            if("/" in f):
-                tmp = f.split(args.dir+"/")[1]
-            elif("\\" in f):
-                tmp = f.split(args.dir+"\\")[1]
-            
-            if(tmp == "/"):
-                tmp.replace("/","")
-            elif(tmp == "\\"):
-                tmp.replace("\\","")
-        '''
+        
         fpath = tmp
         dpath = tmp.replace(fn,"")
         
@@ -64,7 +52,7 @@ def getPaths(args):
         if(dpath != ""):
             darr.append(dpath)
 
-    #arr = {'files':farr,'folders':darr,'original':files};
+    
     arr = []
 
     arr.append(farr)
@@ -73,34 +61,23 @@ def getPaths(args):
 
     return arr
 
-def run():
-    from os import chdir,symlink,getcwd
-    from os import makedirs as mkdir
-    from argsHandle import getArgs
 
-    args = getArgs(["dir","--out","?test"])
+def build(paths,isTest=False,remake=True): 
+    from os import chdir
+    from os import makedirs as mkdir
     
-    paths = getPaths(args)
-    #print(args.test) 
-    build("links")
-    chdir("links")
-    #print(paths)
+    if(not isTest):
+        if(remake):
+            rebuild("links")
+
+        chdir("links")
+    
     for dir in paths[1]:
         print(dir)
-        if(not args.test):
+        if(not isTest):
             try:
                 mkdir(dir)
             except:
                 dummy=""
 
-    i=0
-    for file in paths[0]:
-        print(file+" "+paths[2][i])
-        if(not args.test):
-            try:
-                symlink(paths[2][i],file)
-            except:
-                print("error on file:"+file)
-        i = i+1;
 
-run()
