e03294d373106560f37728167323d4ae3f08ba07
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-21 12:29:34 -0300

    ...

--- test/classtest.py
+++ test/classtest.py
@@ -12,6 +12,15 @@ class Test:
     def print(self,txt):
         print(txt)
 
+class Test2:
+    def test(self):
+        from os import system
+        system("dir")
+
 t = Test("test")
 
 t.print(t.test)
+
+t2 = Test2()
+
+t2.test()
