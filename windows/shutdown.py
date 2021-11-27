47fb17bc8d424ec1f405d3afbfa69a60497f64b0
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-27 10:51:46 -0300

    ...

--- windows/shutdown.py
+++ windows/shutdown.py
@@ -1,15 +1,26 @@
+import sys
+sys.path.append("../../python-lib")
+
+from timeHandle import *
 import time
 from os import system
 from os import chdir
 
 chdir("\\")
 
-shut = int(input("time in hour:"))*60*60
+shut = int(input("time in hour:"))
 
-print(time.ctime())
+t = getTime().split(":")
+t[0] = int(t[0])+shut
+if(t[0] >= 24):
+    t[0]-=24
+t[0] = str(t[0])
+t = t[0]+":"+t[1]+":"+t[2]
 
+shut = shut*60*60
+print("current:"+getTime())
+print("shutdown:"+t)
 time.sleep(shut)
 
-print(time.ctime())
-
+print(getTime())
 system("shutdown /s /f")
