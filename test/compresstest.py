b3f81cf8a22a4a57b98a2936d00941858cc9cffd
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-27 14:14:58 -0300

    ...

--- test/compresstest.py
+++ test/compresstest.py
@@ -1,10 +1,26 @@
 import zlib
+import base65536
 
-def test(fname,b):
-    f = open(fname,"wb")
-    f.write(b)
-    f.close()
+i = 0
+txt = "Hello World!!!"
+temp = txt
+while(i < 100):
+    tmp = base65536.encode(temp.encode("utf-8"))
+    if(len(tmp) < len(txt)):
+        print(len(txt))
+        print(len(tmp))
+        txt = tmp
+        i+=1
+    else:
+        #print(i)
+        temp = tmp
+        print(temp)
 
+#a = base65536.encode(a.encode("utf-8"))
+print(txt)
+#print(base65536.decode(a).decode("utf-8"))
+
+'''
 bytes = "hello world".encode("utf-8")
 tmp = zlib.compress(bytes)
 test("a.txt",tmp)
@@ -15,4 +31,4 @@ test("b.txt",tmp)
 tmp = zlib.compress(tmp)
 #print(tmp)
 test("c.txt",tmp)
-
+'''
