5440b80a88045f86b6b9d5b8db6a139ed028c717
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-28 17:49:19 -0300

    ...

---
+++ test/evalandexectest.py
@@ -0,0 +1,17 @@
+x = eval("2*5+2")
+
+print(x)
+
+exec("def hello(): print('helloworld')")
+
+hello()
+
+exec("x = eval('2*2')")
+
+print(x+1)
+
+exec("def t():\n\treturn 1\ndef t2():\n\treturn 2")
+
+print(t())
+
+print(t2())
