b87cff338663e25d942a2c1e1cd21f6a8f72c8bb
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-30 16:45:02 -0300

    ...

--- capture/test.py
+++ capture/test.py
@@ -7,42 +7,30 @@ from elapse import *
 from os import chdir,mkdir,getcwd
 import socketHandle as sh
 
-#chdir(getTemp())
-
-#remove("capturetest")
-#mkdir("capturetest")
-#chdir("capturetest")
-
-print(getcwd())
-
 def action(s):
-    import base64
     data = sh.receive(s)
-    b = bytes(screenshot(),"png")
-    html = "<img id='ItemPreview' src=''>"
-    html += "<script>"
-    html += 'document.getElementById("ItemPreview").src = "data:image/png;base64,"+"'
-    html = sh.http(html)
-    sh.send(s,html)
-    s.sendall(base64.b64encode(b))
-    html = '";</script>'
-    sh.send(s,html)
+    flag = True
+    
+    while(True):
+        html = "<img id='ItemPreview' src=''>"
+        
+        if(flag):
+            html = sh.http(html)
+            flag = False
+        
+        b = bytes(screenshot(),"jpeg",10)
+        html += "<script>"
+        html += 'document.getElementById("ItemPreview").src = "data:image/jpg;base64,"+"' 
+        sh.send(s,html)
+        s.sendall(b64(b))
+        html = '";</script>'
+        
+        sh.send(s,html)
+        html = ""
+        
+        from time import sleep
+        sleep(0.0165)
+        
+        sh.send(s,"<script>document.write('');</script>")
 
 sh.server(12345,action)
-
-'''
-i=0
-count = 0
-e = Elapse()
-while(True):
-    if(e.elapsed() > 60000):
-        print(count/60)
-        count = 0
-        e = Elapse()
-
-    fn = str(i)#'{:0>3}'.format(i)
-    ss = screenshot()
-    ss.save(fn+".jpg",quality=33)
-    i+=1
-    count+=1
-'''
