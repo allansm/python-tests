7869bba2158bfaa32c6005013b2b762396600568
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-12-01 12:08:05 -0300

    ...

--- capture/test.py
+++ capture/test.py
@@ -6,19 +6,23 @@ from fileHandle import *
 from elapse import *
 from os import chdir,mkdir,getcwd
 import socketHandle as sh
+from time import sleep
 
 def action(s):
     data = sh.receive(s)
     flag = True
     
+    e = Elapse()
+    i = 0
+
     while(True):
-        html = "<img id='ItemPreview' src=''>"
+        html = "<img id='ItemPreview' src='' style='width:640px;height:480px'>"
         
         if(flag):
             html = sh.http(html)
             flag = False
         
-        b = bytes(screenshot(),"jpeg",10)
+        b = bytes(screenshot(),"jpeg",33)
         html += "<script>"
         html += 'document.getElementById("ItemPreview").src = "data:image/jpg;base64,"+"' 
         sh.send(s,html)
@@ -27,10 +31,16 @@ def action(s):
         
         sh.send(s,html)
         html = ""
-        
-        from time import sleep
+         
         sleep(0.0165)
         
         sh.send(s,"<script>document.write('');</script>")
+          
+        if(e.elapsed() > 1000):
+            print("fps:"+str(i))
+            e = Elapse()
+            i=0
+        else:
+            i+=1
 
-sh.server(12345,action)
+sh.server(12345,action)#,"127.0.0.1")
