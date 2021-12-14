0309a544ddb9c30ca4edbcdfaeb6b0ca7f5db964
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-12-14 15:45:02 -0300

    ...

--- tools/encode.py
+++ tools/encode.py
@@ -4,6 +4,7 @@ sys.path.append("../../python-lib")
 
 from argsHandle import *
 from base64 import *
+from util import clear
 
 def b(base,type,data):
     exec("global x;x = b"+base+type+"(data.encode('utf-8')).decode('utf-8')")
@@ -16,19 +17,38 @@ def b(base,type,data):
 
     return tmp
 
-args = getArgs(["--base","data","?decode"])
+def run(args):
+    base = args.base
 
-base = args.base
+    if(base == None):
+        base = "64"
 
-if(base == None):
-    base = "64"
+    data = args.data
+    type = args.decode
 
-data = args.data
-type = args.decode
+    if(not type):
+        type = "encode"
+    else:
+        type = "decode"
 
-if(not type):
-    type = "encode"
+    print(b(base,type,data))
+
+def loop(args):
+    
+    data = args.data
+    
+    while(True):
+        base = input("base:")
+        type = input("type:")
+        
+        clear()
+        data = b(base,type,data)
+        print(data)
+        
+args = getArgs(["--base","data","?decode","?loop"])
+
+if(not args.loop):
+    run(args)
 else:
-    type = "decode"
+    loop(args)
 
-print(b(base,type,data))
