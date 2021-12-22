399d57156eed3660954d84feec7d8a503138f715
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-12-22 14:18:53 -0300

    ...

--- backup/test.py
+++ backup/test.py
@@ -5,27 +5,33 @@ include("../../python-lib")
 from fileHandle import *
 from util import removeEmpty 
 from os import chdir
+from argsHandle import *
+
+args = getArgs(["ref","target"])
+
+ref = args.ref
+target = args.target
 
 files = []
 dirs = []
-for n in getAllFiles("ref"):
+for n in getAllFiles(ref):
     for x in getAllFiles("target"):
-        if n.replace("ref/","") in x:
-            files.append(x.replace("target/",""))
-            dirs.append(basename(dirname(x).replace("target/","").replace("target","")))
+        if n.replace(ref+"/","") in x:
+            files.append(x.replace(target+"/",""))
+            dirs.append(basename(dirname(x).replace(target+"/","").replace(target,"")))
 
 dirs = removeEmpty(dirs)
 files = removeEmpty(files)
-
-#print(files)
-#print(dirs)
 
 remove("backup")
 mkdir("backup")
 chdir("backup")
 
 for n in dirs:
+    print(n)
     mkdir(n)
 
+print("")
 for n in files:
-    copy("../target/"+n,n)
+    print(n)
+    copy("../"+target+"/"+n,n)
