181d3605453bc22895a7e3f16320dce42186737c
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-12-23 13:56:42 -0300

    ...

--- backup/test.py
+++ backup/test.py
@@ -14,14 +14,19 @@ target = args.target
 
 files = []
 dirs = []
+#test = []
 for n in getAllFiles(ref):
-    for x in getAllFiles("target"):
-        if n.replace(ref+"/","") in x:
+    for x in getAllFiles(target):
+        if n.replace(ref+"/","") == x.replace(target+"/",""):
+            fn = getFileName(x)
             files.append(x.replace(target+"/",""))
-            dirs.append(basename(dirname(x).replace(target+"/","").replace(target,"")))
+            #dirs.append(basename(dirname(x).replace(target+"/","").replace(target,"")))
+            dirs.append(x.replace(fn,"").replace(target+"/","").replace(target,""))
 
 dirs = removeEmpty(dirs)
 files = removeEmpty(files)
+#print(dirs)
+#print(test)
 
 remove("backup")
 mkdir("backup")
@@ -35,3 +40,4 @@ print("")
 for n in files:
     print(n)
     copy("../"+target+"/"+n,n)
+
