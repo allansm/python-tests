94e8d45e5e57eab4e4bd2a1be5cce90b220ad199
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-12-26 15:24:45 -0300

    ...

---
+++ hex/test.py
@@ -0,0 +1,30 @@
+from dependency import include
+from base64 import *
+
+include("../../python-lib")
+
+from argsHandle import *
+from fileHandle import *
+
+args = getArgs(["fn","--x","--out"])
+
+fn = args.fn
+x = args.x
+out = args.out
+
+if(out == None):
+    out="out"
+
+if(x != None):
+    x = int(x)
+
+data=""
+
+if(exists(fn)):
+    data = readFile(fn)
+else:
+    data = fn
+
+data = data.replace("\n","").replace(" ","")
+
+writeBytes(out,b16decode(data))
