5aaf14ab7677c991ad242c0432e1df37ea479f89
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-12-29 10:56:23 -0300

    ...

--- app/test.py
+++ app/test.py
@@ -6,20 +6,26 @@ from fileHandle import *
 from argsHandle import *
 from os import system
 
-args = getArgs(["file"])
+args = getArgs(["file","--detail"])
 
-def test(tmp,file):
+def test(tmp,file,detail):
     print(tmp)
+    print(dirname(tmp))
     if(file.lower() in tmp.lower()):
-        system('"start '+tmp+'"')
-        exit()
+        if(detail != None):
+            if(detail in tmp.lower()):
+                system('"start '+tmp+'"')
+                exit()
+        else:
+            system('"start '+tmp+'"')
+            exit()
 
 file = args.file
 for n in getLines(".config"):
     if(args.file in n):
         file = n.split("::")[1]
 
-x = lambda a,b=file : test(a,b)
+x = lambda a,b=file,c=args.detail : test(a,b,c)
 
 for n in getLines(".config"):
     if(not "::" in n):
