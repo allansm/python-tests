39e6ce4dd1d9a8c3c3ba116e3c50cd07eadcd24e
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-02-10 18:30:36 -0300

    ...

--- reddit/test.py
+++ reddit/test.py
@@ -9,17 +9,26 @@ i = 0
 for n in result["data"]["children"]:
     print(str(i)+":"+n["data"]["title"])
     print("comments:"+str(n["data"]["num_comments"]))
-    #print("link:"+n["data"]["permalink"])
     print("")
+
     i+=1
 
 index = int(input("n:"))
 result = getJson("https://www.reddit.com"+result["data"]["children"][index]["data"]["permalink"]+".json")
-#for n in result:
+
 print(result[0]["data"]["children"][0]["data"]["title"])
 print(result[0]["data"]["children"][0]["data"]["selftext"])
 print("")
 
 for n in result[1]["data"]["children"]:
-    print(n["data"]["body"])
+    print(n["data"]["author"]+":")
+    print("\t"+n["data"]["body"])
+    
+    try:
+        for x in n["data"]["replies"]:
+            for replies in x["data"]["children"]:
+                print("\t\t"+replies["data"]["body"])
+    except:
+        dummy=0
+    
     print("") 
