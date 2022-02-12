42b2af416d4b26780fe674ba0c9410b9fe2e6ee4
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-02-12 19:19:42 -0300

    Improve image size.

--- webimageview/test.py
+++ webimageview/test.py
@@ -9,11 +9,29 @@ print(bytes)
 
 root = Tk()
 
-canvas = Canvas(root,width=1366,height=768,highlightthickness=0)
-
 img = Image.open(io.BytesIO(bytes))
+
+w,h = img.size
+
+swidth = root.winfo_screenwidth()
+sheight = root.winfo_screenheight()
+
+if(swidth < w and sheight < h):
+    img = img.resize((swidth,sheight))
+elif(swidth < w):
+    img = img.resize((swidth,h))
+elif(sheight < h):
+    img = img.resize((w,sheight))
+
+w,h = img.size
+
+canvas = Canvas(root,width=w,height=h,highlightthickness=0)
+
 img = ImageTk.PhotoImage(img)
 
-canvas.create_image(1366/2,768/2,anchor="center",image=img)
+root.img = canvas.create_image(w/2,h/2,anchor="center",image=img)
 canvas.pack()
+
+root.resizable(0, 0)
+
 root.mainloop()
