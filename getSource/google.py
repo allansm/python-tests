bf2da3c1eaff0a757e5239f6fdc9863bc4aae2fb
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-02-20 19:47:33 -0300

    Organize data and outputs.

--- getSource/google.py
+++ getSource/google.py
@@ -1,47 +1,51 @@
 from allansm.downloader import getSource
+from allansm.argsHandle import *
 
-code = getSource("https://www.google.com/search?q=python")
+code = getSource("https://www.google.com/search?q="+getArgs(["q"]).q)
 code = code.replace("\n","")
-before = ""
-find = ""
+
+arr = []
+
 for n in code.split("<a "):
     if("href" in n and "/url?" in n):
-        #print("<a "+n+"\n")
-        
         for x in n.split("<div "):
             x = x.replace("\"","\n")
             x = x.replace("'","\n")
             x = x.replace("<","\n")
             x = x.replace(">","\n")
-            #print(x.split("\n"))
-            #print("")
+            
             islink = False
             isText = False
-            if(x.split("\n")[0] == "href="):
+            
+            data = x.split("\n")
+
+            if(data[0] == "href="):
                 islink = True
             if(islink):
-                print(">>>>>>> "+x.split("\n")[1])
+                arr.append(data[1])
             else:
                 for y in x.split("\n"):
-                    if("/h3" in y or "/a" in y):
+                    if("/h3" in y):
                         isText = True
                 if(isText):
-                    print(">>>>>>>> "+x.split("\n")[3])
-            #for y in x.split("\n"):
-                
-            '''
-            for y in x.split("\n"):
-                if("href=" in before):
-                    print(y+"\n\n")
-                elif("h3 " in before):
-                   find = y
-                    #if(y != ""):
-                    #print(y+"\n\n")
-                    #exit()
-                elif(find in before and find != ""):
-                    print(y+"\n")
-                    exit()
-                #else:
-                    #print(before)
-                
-                before = y'''
+                    arr.append(data[3])
+
+data = {"title":"","links":[]}
+full = []
+
+for n in arr:
+    if(not "wiki" in n.lower()):
+        if(not "url?" in n):
+            data["title"] = n
+            full.append(data)
+            data = {"title":"","links":[]}
+        else:
+            data["links"].append(n.replace("/url?q=",""))
+
+for n in full:
+    if(len(n["links"]) > 0):
+        print(n["title"])
+        for x in n["links"]:
+            print(x)
+        print("")
+
