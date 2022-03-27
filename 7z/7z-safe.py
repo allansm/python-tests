b26f7e08aa1806a3b984b573315172f7b55b5ded
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-03-27 16:31:14 -0300

    Improve security and rename file.

--- 7z/7z-safe.py
+++ 7z/7z-safe.py
@@ -6,15 +6,17 @@ from getpass import getpass
 
 args = getArgs(["path"])
 
-pw = getpass("password:")
 if(isdir(args.path)):
-    dname = realpath(args.path)
-    fname = "../"+dname.replace("\\","/").split("/")[-1].split("7z")[0]+".7z"
-    chdir(dname)
-    exec("7z a "+fname+" . -p"+pw)
-    chdir("..")
-    remove(dname)
-
+    pw = getpass("password:")
+    if(pw == getpass("confirm password:")):
+        dname = realpath(args.path)
+        fname = "../"+dname.replace("\\","/").split("/")[-1].split("7z")[0]+".7z"
+        chdir(dname)
+        exec("7z a "+fname+" . -p"+pw)
+        chdir("..")
+        remove(dname)
+    else:
+        print("incorrect password try again.")
 else:
     fname = realpath(args.path)
     dname = fname.replace("\\","/").split("/")[-1].split("7z")[0]
@@ -23,5 +25,6 @@ else:
     mkdir(dname)
     chdir(dname)
     
-    exec("7z x "+fname+" -p"+pw)
+    print("password:", end="\r")
+    exec("7z x "+fname)
     remove(fname)
