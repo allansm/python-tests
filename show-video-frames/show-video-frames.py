73bdfc94b7f48ff890a1daea4a8e5350cee183ca
__CODER <allan.de.souza.melo.rj@gmail.com>
2022-04-08 23:37:53 -0300

    Keep proportion when rescale

--- show-video-frames/show-video-frames.py
+++ show-video-frames/show-video-frames.py
@@ -3,8 +3,26 @@ from tkinter import *
 from allansm.opencv import *
 from PIL import Image, ImageTk
 
+def size(event):
+    global current, frames, canvas, lastimg, root
+    
+    img = Image.fromarray(frames[current])
+    
+    w, h = img.size
+    while(w > root.winfo_width()):    
+        img = img.resize((int(w*0.9), int(h*0.9)))
+        w, h = img.size
+
+    img = ImageTk.PhotoImage(img)
+    
+    canvas.delete('all')
+    canvas.config(width=root.winfo_width(), height=root.winfo_height())
+    canvas.create_image(root.winfo_width()/2, root.winfo_height()/2, image=img)
+     
+    lastimg = img
+
 def l(event):
-    global current, frames, canvas, lastimg
+    global current, frames, canvas, lastimg, root
 
     current-=1
 
@@ -12,24 +30,42 @@ def l(event):
         current = 0
     
     img = Image.fromarray(frames[current])
-    img = ImageTk.PhotoImage(img)
-    canvas.create_image(1366/2, 768/2, image=img)
     
+    w, h = img.size
+    while(w > root.winfo_width()):    
+        img = img.resize((int(w*0.9), int(h*0.9)))
+        w, h = img.size
+
+    img = ImageTk.PhotoImage(img)
+    
+    canvas.delete('all')
+    canvas.config(width=root.winfo_width(), height=root.winfo_height())
+    canvas.create_image(root.winfo_width()/2, root.winfo_height()/2, image=img)
+     
     lastimg = img
 
     print("frame:"+str(current))
 
 def r(event):
-    global current, frames, canvas, lastimg
+    global current, frames, canvas, lastimg, root
 
     current+=1
 
-    if(current > len(frames)):
-        current = len(frames)
+    if(current > len(frames)-1):
+        current = len(frames)-1
     
     img = Image.fromarray(frames[current])
+    
+    w, h = img.size
+    while(w > root.winfo_width()):    
+        img = img.resize((int(w*0.9), int(h*0.9)))
+        w, h = img.size
+
     img = ImageTk.PhotoImage(img)
-    canvas.create_image(1366/2, 768/2, image=img)
+    
+    canvas.delete('all')
+    canvas.config(width=root.winfo_width(), height=root.winfo_height())
+    canvas.create_image(root.winfo_width()/2, root.winfo_height()/2, image=img)
     
     lastimg = img
 
@@ -39,22 +75,33 @@ lastimg = None
 current = 0
 
 root = Tk()
+root.title("show-video-frames")
+root.geometry("640x480")
 
 file = openfile()
 frames = getFrames(file)
 
 print(len(frames))
 
-canvas = Canvas(root, width=1366, height=768, highlightthickness=0)
+canvas = Canvas(root, width=640, height=480, highlightthickness=0)
+canvas.config(bg="black")
 canvas.pack(fill="both", expand=True)
 
 img = Image.fromarray(frames[0])
+
+w, h = img.size
+while(w > root.winfo_width()):    
+    img = img.resize((int(w*0.9), int(h*0.9)))
+    w, h = img.size    
+
 img = ImageTk.PhotoImage(img)
 
-canvas.create_image(1366/2, 768/2, image=img)
+canvas.create_image(640/2, 480/2, image=img)
 
 root.bind("<Left>", l)
 root.bind("<Right>", r)
+root.bind("<Configure>", size)
+
 root.after(1, lambda: root.focus_force())
 
 root.mainloop()
