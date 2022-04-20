dbbf14bb1ca9827d639b4b56f8733cc662bba618
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-04-20 20:03:58 -0300

    Add resume arg

--- capture/capture-loop.py
+++ capture/capture-loop.py
@@ -1,10 +1,12 @@
 from allansm.imageHandle import *
-from allansm.fileHandle import getTemp,mkdir,remove
+from allansm.fileHandle import getTemp, mkdir, remove, getFileName, ls
+from allansm.argsHandle import *
 from os import chdir,getcwd
 from time import sleep
-from allansm.argsHandle import *
 
-interval = getArgs(["--interval"]).interval
+args = getArgs(["--interval", "?resume"])
+
+interval = args.interval
 
 if(interval == None):
     interval = 1
@@ -12,13 +14,24 @@ else:
     interval = float(interval)
 
 chdir(getTemp())
-remove("capture-loop")
+
+if(not args.resume):
+    remove("capture-loop")
+
 mkdir("capture-loop")
 chdir("capture-loop")
 
 print(getcwd())
 
 i=0
+
+if(args.resume):
+    for n in ls():
+        h = int(getFileName(n).split(".")[0])
+
+        if(h > i):
+            i=h
+
 while(True):
     print(i)
     screenshot().save(str(i)+".png")
