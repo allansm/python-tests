6a870b131d56dfceb302170dbb85d253299a6c81
Allan De Souza Melo <allan.de.souza.melo.rj@gmail.com>
2022-08-07 15:05:17 -0300

    Create disk.py

---
+++ linux/disk.py
@@ -0,0 +1,18 @@
+from subprocess import check_output
+
+out = check_output("df -h | awk '{print $1\" \"$5}'", shell=True).decode()
+lines = out.split("\n")
+
+del lines[0]
+
+for n in lines:
+    data = n.split(" ")
+    path = data[0]
+    if(path != "tmpfs"):
+        percent = float(data[1].replace("%", ""))
+        diff = 15 - len(path)
+
+        print(path, end=" "*diff)
+        print("█"*round(percent/5), end="")
+        print("░"*round((100-percent)/5),end="")
+        print(" "+str(percent)+"%\n")
