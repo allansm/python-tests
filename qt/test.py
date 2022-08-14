4b8b0648b276704b301defb3da226cc5e12388e0
Allan De Souza Melo <allan.de.souza.melo.rj@gmail.com>
2022-08-14 14:28:41 -0300

    Test qt

---
+++ qt/test.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel
+
+def test():
+    global input
+
+    print(input.text())
+    input.setText("")
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+
+input = QLineEdit(window)
+input.setGeometry(10, 30, 150, 25)
+
+button = QPushButton("click me", window)
+button.setGeometry(170, 30, 70, 25);
+
+label = QLabel("Name:", window)
+label.setGeometry(10, 10, 50, 15)
+
+button.clicked.connect(test)
+
+window.resize(250, 65)
+window.setMaximumSize(250, 60);
+window.setWindowTitle("Test");
+window.show()
+
+sys.exit(app.exec_())
